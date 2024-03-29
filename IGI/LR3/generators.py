7325cebd3331a6f73f577629e2d5ea47df9dcf56
Lovely Paul <ppetrov228@bk.ru>
2024-03-29 15:15:58 +0300

    Исправлены ошибки в отчёте

--- IGI/LR3/generators.py
+++ IGI/LR3/generators.py
@@ -2,10 +2,11 @@ import random
 import string
 
 
-def int_sequence(size: int) -> list[int]:
+def int_sequence(size: int):
     ''' Function for generating int sequence of arbitrary size\
 Numbers are generated in the interval [-150, 150]. '''
-    return [random.randint(-150, 150) for i in range(size)]
+    for i in range(size):
+        yield random.randint(-150, 150)
 
 
 def literal_sequence(size: int) -> str:
@@ -14,7 +15,8 @@ Using all printable ASCII characters. '''
     return ''.join(random.choices(string.printable, k=size))
 
 
-def float_sequence(size: int) -> list[float]:
+def float_sequence(size: int):
     ''' Function for generating float sequence of arbitrary size\
 Numbers are generated in the interval [-10, 10] with 2 decimal places. '''
-    return [float(format(random.uniform(-10, 10), '.2f')) for i in range(size)]
+    for i in range(size):
+        yield float(format(random.uniform(-10, 10), '.2f'))
